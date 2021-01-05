f5c379be29563659973e25b7ab855775bd14c7b7
PortFolio <leemember.git.GitHub@gmail.com>
2021-01-05 19:54:13 +0900

    한줄로 api를 불러와 이전과 똑같은 기능 구현

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -18,43 +18,8 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 //thunk 함수를 생성합니다.
 //thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치 합니다.
 
-export const getPost = id => async dispatch => {
-    dispatch({ type : GET_POST }); // 요청을 시작한 것을 알린다.
-    try {
-        const response = await api.getPost(id);
-        dispatch({
-            type: GET_POST_SUCCESS,
-            payload: response.data
-        }); // 요청 성공시 데이터가 응답
-    } catch (e) {
-        dispatch({
-            type: GET_POST_FAILURE,
-            payload: e,
-            error: true,
-        }); // 요청 실패시 에러 발생
-        throw e;
-        //나중에 컴포넌트단에서 에러를 조회할 수 있게 해준다.
-    }
-};
-
-export const getUsers = () => async dispatch => {
-    dispatch({ type : GET_USERS }); // 요청을 시작한 것을 알린다.
-    try {
-        const response = await api.getUsers();
-        dispatch({
-            type: GET_USERS_SUCCESS,
-            payload: response.data
-        }); // 요청 성공시 데이터가 응답
-    } catch (e) {
-        dispatch({
-            type: GET_USERS_FAILURE,
-            payload: e,
-            error: true,
-        }); // 요청 실패시 에러 발생
-        throw e;
-        //나중에 컴포넌트단에서 에러를 조회할 수 있게 해준다.
-    }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 // 초기상태 선언하기.
 // 요청의 로딩 중 상태는 loading 이라는 객체에서 관리합니다.
